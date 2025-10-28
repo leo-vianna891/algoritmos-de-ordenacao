a56a7098056c43d7d20977ad688de6036332cdd5
faustus <vitalbfausto@gmail.com>
2025-10-28 17:40:53 -0300

    Main completa, mas continuaremos fazendo revisões.

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include "algoritmos.c"
 
-const int max_int = 1e5;
+const int max_int = 1e8;
 
 void menu0() {
 
@@ -15,7 +16,17 @@ void menu0() {
 
 }
 
-void menu() {
+void menu1() {
+
+    printf("-=-=-= Agora, escolha o estado inicial do vetor -=-=-=\n");
+    printf("[1] Ordenado\n");
+    printf("[2] Reverso\n");
+    printf("[3] Aleatório\n");
+
+    printf("=-=-=-=-=-=-=\n");
+}
+
+void menu2() {
     printf("-=-=-= Escolha qual algoritmo de ordenação será usado: =-=-=-\n");
 
     printf("[1] BubbleSort\n");
@@ -32,21 +43,8 @@ void menu() {
     printf("=-=-=-=-=-=-=\n");
 }
 
-void menu2() {
-
-    printf("-=-=-= Agora, escolha o estado inicial do vetor -=-=-=\n");
-    printf("[1] Ordenado\n");
-    printf("[2] Reverso\n");
-    printf("[3] Aleatório\n");
-
-    printf("=-=-=-=-=-=-=\n");
-}
-
 void init_vet(int tam, int vet[], int type) {
     
-    // Assegura que os números aleatórios sejam diferentes em cada execução
-    srand(time(NULL)); 
-
     switch (type) {
         
         // 1: Ordenado (Crescente)
@@ -61,16 +59,14 @@ void init_vet(int tam, int vet[], int type) {
 
         // 3: Aleatório
         case 3:
-            for (int i = 0; i < tam; i++) {
-                
-                // Gera números aleatórios de 1 até 10^5 (mais que isso pode dar problema nos algoritmos contagem de menores e radix)
-                vet[i] = (rand() % (max_int)) + 1; 
-            }
+            for (int i = 0; i < tam; i++) vet[i] = (rand() % (max_int)) + 1; 
             break;
     }
 }
 
 int main() {
+
+    srand(time(NULL));
 
     while (true) {
 
@@ -79,6 +75,7 @@ int main() {
         int tam;
         scanf("%d", &tam);
 
+        //Tratamento da entrada
         if (tam == -1) break;
         while (tam < 1 || tam > 4) {
 
@@ -93,11 +90,14 @@ int main() {
             case 3: tam = 10000; break;
             case 4: tam = 100000; break;
         }
-        int vet[tam];
+        int *vet = (int*) malloc(sizeof(int) * tam); //Alocando na heap para evitar seg fault (principalmente no ContagemDosMenores)
+        
+
+
 
 
         //Escolha do estado inicial do vetor
-        menu2();
+        menu1();
         int estado;
         scanf("%d", &estado);
 
@@ -111,8 +111,13 @@ int main() {
         int reps = 1;
         if (estado == 3) reps = 5; //5 vezes para um vetor aleatório
 
+        
+
+
+
+
         //Escolha do algoritmo de ordenação
-        menu();
+        menu2();
         int alg;
         scanf("%d", &alg);
 
@@ -122,41 +127,65 @@ int main() {
             scanf("%d", &alg);
         }
 
+
+
+
+
+
+        //Variáveis para medir o tempo e fazer contagens
+        clock_t start, end;
+        double tempo = 0.0;
+        ll mov = 0, comp = 0; //Movimentos e comparações
+
+        bool correto = true; 
         for (int i = 1; i <= reps; i++) {
 
             //Em cada repetição, vamos repopular o vetor (relevante apenas para o caso de vetor aleatório)
-            switch (estado) {
-                case 1: init_vet(tam, vet, 1); break;
-                case 2: init_vet(tam, vet, 2); break;
-                case 3: init_vet(tam, vet, 3); break;
-            }
+            init_vet(tam, vet, estado);
             
+            start = clock();
             switch (alg) {
 
-                case 1: bubbleSort(vet, tam); break;
-                case 2: selectionSort(vet, tam); break;
-                case 3: insertionSort(vet, tam); break;
-                case 4: shellSort(vet, tam); break;
-                case 5: quickSort(vet, tam); break;
-                case 6: heapSort(vet, tam); break;
-                case 7: mergeSort(vet, tam); break;
-                case 8: contagemDosMenores(vet, tam); break;
-                case 9: radixSort(vet, tam); break;
+                case 1: bubbleSort(vet, tam, &comp, &mov); break;
+                case 2: selectionSort(vet, tam, &comp, &mov); break;
+                case 3: insertionSort(vet, tam, &comp, &mov); break;
+                case 4: shellSort(vet, tam, &comp, &mov); break;
+                case 5: quickSort(vet, tam, &comp, &mov); break;
+                case 6: heapSort(vet, tam, &comp, &mov); break;
+                case 7: mergeSort(vet, tam, &comp, &mov); break;
+                case 8: contagemDosMenores(vet, tam, &comp, &mov); break;
+                case 9: radixSort(vet, tam, &comp, &mov); break;
             }
+            end = clock();
+
+            tempo += (double) (end - start) / CLOCKS_PER_SEC;
                         
-                //Verificação da ordenação (crescente)
-                int correto = true;
-                for (int i = 1; i < tam; i++) {
-
-                    //printf("%d ", vet[i]); //se preferir, descomente para imprimir tudo
-
-                    if (vet[i-1] > vet[i]) 
-                        correto = false;
-                }
-                printf("\n");
-
-                if (correto) printf("Ordenação CORRETA!\n\n");
-                else printf("Ordenação ERRADA!\n\n");
         }
+
+        //Resultados médios
+        tempo /= reps;
+        comp /= reps;
+        mov /= reps;
+
+        printf("=-=-=- RESULTADOS -=-=-=\n");
+        //Verificação da ordenação (crescente)
+        for (int i = 1; i < tam; i++) {
+
+            //printf("%d ", vet[i]); //se preferir, descomente para imprimir tudo
+
+            if (vet[i-1] > vet[i]) 
+                correto = false;
+        }
+        printf("\n");
+        free(vet);
+
+        if (correto) printf("Ordenação realizada com sucesso.\n");
+        else printf("Um erro de ordenação foi encontrado.\n");
+
+        printf("O algoritmo foi executado %d vez(es).\n", reps);
+        printf("Número de Comparações: %lld\n", comp);
+        printf("Número de Movimentos: %lld\n", mov);
+        printf("Tempo (médio) de execução: %.6lf segundo(s).\n", tempo);
+
     }
 }
